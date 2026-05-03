9456d7a16eb21712bbedab1c27a2e2e7c7ed5906
Mohamed Magdy <midomohamedmagdy@hotmail.com>
2026-05-03 13:03:32 +0300

    Minor fix

--- src/app/(dashboard)/(admin)/dashboard/audit-log/_components/audit-log.tsx
+++ src/app/(dashboard)/(admin)/dashboard/audit-log/_components/audit-log.tsx
@@ -66,7 +66,7 @@ export default function AdminAuditlogComponent({ role }: { role: string }) {
                     </Button>
                 </div>
 
-                {role !== 'SUPER ADMIN' && <div>
+                {role === 'SUPER ADMIN' && <div>
                     <Dialog>
                         <DialogTrigger asChild>
                             <Button variant={"destructive"}
